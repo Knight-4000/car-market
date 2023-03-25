cfd3e49e9361a332d23de84f9642d7237ab8876f
Knight-4000 <yorksprinters@gmail.com>
2023-03-25 11:27:08 -0400

    Added links, css to header.

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,9 +5,10 @@ import { Link } from 'react-router-dom';
 const logo = (
   <div className='logo'>
     <Link to="/">
-      <h2 className='text-2xl'>
-        Car<span>Market</span>
+      <h2 className='text-2xl logo-one text-white'>
+        Car<span className='logo-two'>Market</span>
       </h2>
+      <p className='tagline'>Shop Online. Stay Home.</p>
     </Link>
 </div>
 )
@@ -17,6 +18,22 @@ const Header = () => {
     <header>
       <div className='header'>
         {logo}
+        <nav>
+          <ul className='list-none mx-auto hidden md:block'>
+            <Link to="/" className='px-2'>
+              Home
+            </Link>
+            <Link to="/contact" className='px-2'>
+              Contact
+            </Link>
+          </ul>
+          <div className='user-links'>
+            <span className='links hidden md:block'>
+              <Link to="/login" className='px-2'>Login</Link>
+              <Link to="/register" className='px-2'>Register</Link>
+            </span>
+          </div>
+        </nav>
       </div> 
     </header>
   )
